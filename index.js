92ba70a15c24a599076a8f199fd10003c9e74e7c
Khaled-994 <kalkhaled1994@gmail.com>
2020-12-28 19:56:08 +0100

    basic heroku

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express =require('express');
 const routes = require('./routes')
 const app = express();
-const PORT =5050
+const PORT =process.env.PORT || 5050
 app.use(express.static('public'))
 app.set('view engine' , 'ejs')
 
 
-const homeSection3 = require('./homeSection3.json');
-const router = require('./routes');
+// const homeSection3 = require('./homeSection3.json');
+// const router = require('./routes');
 app.use(routes)
 
 
